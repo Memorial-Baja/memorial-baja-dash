017f6eddccfaefbf3dd9907267a72bbd6318448f
Liam Earle <liamearle10@gmail.com>
2024-12-01 12:20:45 -0500

    Logo Screen

--- src/screen_logo.c
+++ src/screen_logo.c
@@ -32,20 +32,6 @@
 static int framesCounter = 0;
 static int finishScreen = 0;
 
-static int logoPositionX = 0;
-static int logoPositionY = 0;
-
-static int lettersCount = 0;
-
-static int topSideRecWidth = 0;
-static int leftSideRecHeight = 0;
-
-static int bottomSideRecWidth = 0;
-static int rightSideRecHeight = 0;
-
-static int state = 0;              // Logo animation states
-static float alpha = 1.0f;         // Useful for fading
-
 Image logo;
 Texture2D texture;
 //----------------------------------------------------------------------------------
@@ -55,77 +41,19 @@ Texture2D texture;
 // Logo Screen Initialization logic
 void InitLogoScreen(void)
 {
-    logo = LoadImage("resources/baja_logo_1.png");
+    logo = LoadImage("resources/baja_logo.png");
     texture = LoadTextureFromImage(logo);
     finishScreen = 0;
     framesCounter = 0;
-    lettersCount = 0;
-
-    logoPositionX = GetScreenWidth()/2 - 128;
-    logoPositionY = GetScreenHeight()/2 - 128;
-
-    topSideRecWidth = 16;
-    leftSideRecHeight = 16;
-    bottomSideRecWidth = 16;
-    rightSideRecHeight = 16;
-
-    state = 0;
-    alpha = 1.0f;
 }
 
 // Logo Screen Update logic
 void UpdateLogoScreen(void)
 {
-    finishScreen = 1;
-    if (state == 0)                 // State 0: Top-left square corner blink logic
-    {
+    if (framesCounter > 100) {
+        finishScreen = 1;
+    } else {
         framesCounter++;
-
-        if (framesCounter == 80)
-        {
-            state = 1;
-            framesCounter = 0;      // Reset counter... will be used later...
-        }
-    }
-    else if (state == 1)            // State 1: Bars animation logic: top and left
-    {
-        topSideRecWidth += 8;
-        leftSideRecHeight += 8;
-
-        if (topSideRecWidth == 256) state = 2;
-    }
-    else if (state == 2)            // State 2: Bars animation logic: bottom and right
-    {
-        bottomSideRecWidth += 8;
-        rightSideRecHeight += 8;
-
-        if (bottomSideRecWidth == 256) state = 3;
-    }
-    else if (state == 3)            // State 3: "raylib" text-write animation logic
-    {
-        framesCounter++;
-
-        if (lettersCount < 10)
-        {
-            if (framesCounter/12)   // Every 12 frames, one more letter!
-            {
-                lettersCount++;
-                framesCounter = 0;
-            }
-        }
-        else    // When all letters have appeared, just fade out everything
-        {
-            if (framesCounter > 200)
-            {
-                alpha -= 0.02f;
-
-                if (alpha <= 0.0f)
-                {
-                    alpha = 0.0f;
-                    finishScreen = 1;   // Jump to next screen
-                }
-            }
-        }
     }
 }
 
@@ -133,6 +61,7 @@ void UpdateLogoScreen(void)
 void DrawLogoScreen(void) {
     DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), BLACK);
     DrawTexture(texture, GetScreenWidth()/2 - texture.width/2, GetScreenHeight()/2 - texture.height/2 - 40, WHITE);
+    DrawText("Loading...", GetScreenWidth()/2-(MeasureTextEx(font, "Loading...", 40, 2).x)/2.0f, GetScreenHeight()/2 + 160, 40, RAYWHITE);
 }
 
 // Logo Screen Unload logic
